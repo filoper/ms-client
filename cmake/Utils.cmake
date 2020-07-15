9924f2c94832c6c558da36e7141c36afee3ae6c0
filoper <67063901+filoper@users.noreply.github.com>
2020-07-15 09:17:44 +0200

    Fix build target clang-format

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -13,7 +13,37 @@ function(add_clang_format_target)
     if(${PROJECT_NAME}_CLANG_FORMAT_BINARY)
     add_custom_target(clang-format
         COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-        -i ${CMAKE_CURRENT_LIST_DIR}/${sources} ${CMAKE_CURRENT_LIST_DIR}/${headers})
+        -i 
+        --style=file --verbose 
+        ${CMAKE_CURRENT_LIST_DIR}/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Audio/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Audio/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Character/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Character/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Character/*/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Character/*/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Data/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Data/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Gameplay/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Gameplay/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Gameplay/*/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Gameplay/*/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Graphics/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Graphics/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/IO/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/IO/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/IO/*/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/IO/*/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*/*/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Net/*/*/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Template/*.h
+        ${CMAKE_CURRENT_LIST_DIR}/Util/*.cpp 
+        ${CMAKE_CURRENT_LIST_DIR}/Util/*.h)
     message("Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).\n")
     endif()
 endfunction()

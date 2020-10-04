465a3187020171152b581044e50734b6d5bb13f6
filoper <67063901+filoper@users.noreply.github.com>
2020-10-04 12:32:25 +0200

    Fix UI char info
    * disable trade button if self
    * toggle item window and bottom windows

--- IO/UITypes/UICharInfo.cpp
+++ IO/UITypes/UICharInfo.cpp
@@ -389,15 +389,18 @@ void UICharInfo::update_stats(int32_t character_id,
                               const std::string &a) {
     int32_t player_id = Stage::get().get_player().get_oid();
 
-    if (character_id == player_id) {
-        buttons_[Buttons::BT_PARTY]->set_state(Button::State::DISABLED);
-        buttons_[Buttons::BT_POP_DOWN]->set_state(Button::State::DISABLED);
-        buttons_[Buttons::BT_POP_UP]->set_state(Button::State::DISABLED);
-        buttons_[Buttons::BT_FRIEND]->set_state(Button::State::DISABLED);
-    }
+    auto bt_state = character_id == player_id ? Button::State::DISABLED
+                                              : Button::State::NORMAL;
+
+    buttons_[Buttons::BT_PARTY]->set_state(bt_state);
+    buttons_[Buttons::BT_TRADE]->set_state(bt_state);
+    buttons_[Buttons::BT_POP_DOWN]->set_state(bt_state);
+    buttons_[Buttons::BT_POP_UP]->set_state(bt_state);
+    buttons_[Buttons::BT_FRIEND]->set_state(bt_state);
 
     Job character_job = Job(job_id);
 
+    target_character_ = &Stage::get().get_character(character_id)->get();
     name_.change_text(target_character_->get_name());
     job_.change_text(character_job.get_name());
     level_.change_text(std::to_string(lv));
@@ -410,22 +413,30 @@ void UICharInfo::update_stats(int32_t character_id,
 }
 
 void UICharInfo::show_bottom_window(uint16_t buttonid) {
+    bool was_personality_enabled = personality_enabled_;
+    bool was_collect_enabled = collect_enabled_;
+    bool was_damage_enabled = damage_enabled_;
     personality_enabled_ = false;
     collect_enabled_ = false;
     damage_enabled_ = false;
 
     switch (buttonid) {
-        case Buttons::BT_PERSONALITY: personality_enabled_ = true; break;
-        case Buttons::BT_COLLECT: collect_enabled_ = true; break;
-        case Buttons::BT_DAMAGE: damage_enabled_ = true; break;
+        case Buttons::BT_PERSONALITY:
+            personality_enabled_ = !was_personality_enabled;
+            break;
+        case Buttons::BT_COLLECT:
+            collect_enabled_ = !was_collect_enabled;
+            break;
+        case Buttons::BT_DAMAGE: damage_enabled_ = !was_damage_enabled; break;
     }
 }
 
 void UICharInfo::show_right_window(uint16_t buttonid) {
+    bool was_item_enabled = item_enabled_;
     item_enabled_ = false;
 
     switch (buttonid) {
-        case Buttons::BT_ITEM: item_enabled_ = true; break;
+        case Buttons::BT_ITEM: item_enabled_ = !was_item_enabled; break;
     }
 }
 }  // namespace ms
